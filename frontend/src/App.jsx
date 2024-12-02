cdf041789fe05c2d223d7782cd6d09cda47b4a36
Joshua Sol <joshuasol4950@gmail.com>
2024-12-03 00:27:50 +0800

    adding routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home.jsx";
+import Login from "./components/Login.jsx";
+import Register from "./components/Register.jsx";
+import Buyer from "./components/Buyer.jsx";
+import Seller from "./components/Seller.jsx";
+
 const App = () => {
   return (
-    <>
-      <div className="text-blue-600 text-xl">Home</div>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+
+      {/*protected routes */}
+      <Route path="buyer" element={<Buyer />} />
+      <Route path="seller" element={<Seller />} />
+    </Routes>
   );
 };
 
