d86155962b4658cd642bb61e86a297d6576e7338
Joshua Sol <joshuasol4950@gmail.com>
2024-12-11 22:52:52 +0800

    Nav Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header.jsx";
 import Home from "./pages/Home.jsx";
+import Category from "./pages/Category.jsx";
+import Cart from "./pages/Cart.jsx";
+import Product from "./pages/Product.jsx";
+import Login from "./pages/Login.jsx";
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
   return (
@@ -9,7 +14,16 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/mens" element={<Category />} />
+          <Route path="/womens" element={<Category />} />
+          <Route path="/kids" element={<Category />} />
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+          <Route path="/cart-page" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </main>
   );
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 max_padd_container m-auto w-full bg-white ring-1 ring-slate-900/5 z-10 ">
+    <header className="fixed top-0 max_padd_container mx-auto w-full bg-white py-10 z-10 ">
       <div className=" py-4 flex justify-between items-center">
         {/*logo*/}
         <div>
