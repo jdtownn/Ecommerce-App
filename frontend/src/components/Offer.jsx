f91a60d6510a2955d6e77d99fadb0f2d133c4988
Joshua Sol <joshuasol4950@gmail.com>
2024-12-13 10:15:36 +0800

    Offers Section

---
+++ frontend/src/components/Offer.jsx
@@ -0,0 +1,13 @@
+const Offer = () => {
+  return (
+    <section className="bg-banneroffer bg-cover bg-center w-full px-4 py-24 mt-16">
+      <div className="max_padd_container">
+        <h2 className="h2">Holiday Season Sale 40% Off</h2>
+        <h3 className="h3 capitalize">Men&apos;s Leather Shoes</h3>
+        <button className="btn_dark_rounded">Go to Store</button>
+      </div>
+    </section>
+  );
+};
+
+export default Offer;
