c086f1c6e430caa215faaa3bf119714ba25f0e0b
Joshua Sol <joshuasol4950@gmail.com>
2024-12-13 09:15:17 +0800

    Popular Section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
+import Popular from "../components/Popular";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <Popular />
     </>
   );
 };
